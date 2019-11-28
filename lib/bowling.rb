c37da33920fb1aea7adc8433b691f7b17d6c4142
EC2 Default User <ec2-user@ip-172-31-19-142.us-west-2.compute.internal>
2019-11-28 20:58:48 +0000

    tori1

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,6 +1,8 @@
 class Bowling
 	def initialize
 		@total_score = 0
+		@scores = []
+		@temp = []
 	end
 
 	def total_score
@@ -8,6 +10,21 @@ class Bowling
 	end
 
 	def add_score(pins)
-		@total_score += pins
+		@temp << pins
+		if @temp.size == 2
+			@socres = @temp
+			@temp = []
+		end
 	end
+
+	def calc_score
+		@scores.each.with_index(1) do |score,index|
+			if score.inject(:+) == 10 && index < 10
+				@total_score += 10 + @scores[index].first
+			else
+				@total_score += @scores.inject(:+)
+			end
+		end
+	end
+
 end
