c5b8f4766b564986e26dfa07b83a71d83108cb9c
EC2 Default User <ec2-user@ip-172-31-19-142.us-west-2.compute.internal>
2019-11-29 15:23:50 +0000

    aa

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -8,18 +8,25 @@ class Bowling
 	def total_score
 		@total_score
 	end
-
+	#スコアを追加する
 	def add_score(pins)
 		@temp << pins
-		if @temp.size == 2
-			@socres = @temp
+		if @temp.size == 2 || strike?(@temp)
+			@socres << @temp
 			@temp = []
 		end
 	end
-
+	#スコアの合計を計算する
 	def calc_score
 		@scores.each.with_index(1) do |score,index|
-			if spare?(index) && not_last_frame?(index)
+			
+			if strike?(score) && not_last_frame?(index)
+				if strike?(@scores[index]) && not_last_frame?(index + 1)
+					@total_score += 20 + @scores[index + 1].first
+				else
+					@toral_score += 10 + @scores[inedx].inject[:+]
+				end
+			elsif spare?(index) && not_last_frame?(index)
 				@total_score += calc_spare_bonus(index)
 			else
 				@total_score += @scores.inject(:+)
@@ -39,6 +46,10 @@ class Bowling
 
 	def calc_spare_bonus(index)
 		10 + @scores[index].first
+	end
+	
+	def strike?(score)
+		score.first == 10
 	end
 
 end
